f10bab01395672f93e6728006d6420d6906213b6
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-03-29 19:51:27 +0200

    Home page gallery done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,20 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'opaque-20': 'rgba(0,0,0,0.2)',
+        'opaque-30': 'rgba(0,0,0,0.3)',
+        'opaque-40': 'rgba(0,0,0,0.4)',
+        'opaque-50': 'rgba(0,0,0,0.5)',
+        'opaque-60': 'rgba(0,0,0,0.6)',
+        'opaque-80': 'rgba(0,0,0,0.8)',
+        'opaque-90': 'rgba(0,0,0,0.9)',
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
   },
   variants: {},
   plugins: [],
