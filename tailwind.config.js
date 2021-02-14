4a8cf3a7763ce117ae8c8a41dafa7a5bcf450925
Touhidur Rahman <touhidur.rahman@check24.de>
2021-02-14 14:30:19 +0100

    Update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      blue: colors.cyan, // primary
+      green: colors.green, // success
+      yellow: colors.amber, // warn
+      red: colors.orange, // danger
+      gray: colors.trueGray,
+    },
+    extend: {
+
+    },
   },
   variants: {},
   plugins: [],
